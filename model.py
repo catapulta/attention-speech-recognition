eee33306c44f4bda6c9726bc0d057e9337c17cc8
Aldo <amarinim@andrew.cmu.edu>
2018-11-25 00:15:36 -0500

    add files

---
+++ model.py
@@ -0,0 +1,239 @@
+from torch import nn
+from torch.nn.utils import rnn
+import torch
+import torch.nn.functional as F
+import pdb
+import numpy as np
+
+
+# Model that takes packed sequences in training
+class EncoderCNN(nn.Module):
+    def __init__(self, key_size=128, value_size=256, nlayers=3, hidden_size=512,
+                 cnn_compression=2, bidirectional=True):
+        super(EncoderCNN, self).__init__()
+        self.cnn_compression = cnn_compression
+        self.key_size = key_size
+        self.value_size = value_size
+        self.kv_size = key_size + value_size
+        self.bidirectional = bidirectional
+        self.directions = 2 if bidirectional else 1
+        self.rnn_input_size = 1280
+        self.hidden_size = hidden_size
+        self.nlayers = nlayers
+        self.cnn = nn.Sequential(
+            nn.Conv2d(1, 16, kernel_size=3, stride=(1, 2), padding=(1, 1), bias=False),
+            nn.BatchNorm2d(16),
+            nn.ELU(),
+            nn.Conv2d(16, 32, kernel_size=3, stride=(1, 1), padding=(1, 1), bias=False),
+            nn.BatchNorm2d(32),
+            nn.ELU(),
+            nn.Conv2d(32, 64, kernel_size=3, stride=(2, 2), padding=(1, 1), bias=False),
+            nn.ELU(),
+            nn.Conv2d(64, self.rnn_input_size // 10, kernel_size=3, stride=(1, 1), padding=(1, 1), bias=False),
+            nn.BatchNorm2d(self.rnn_input_size // 10),
+            nn.ELU()
+        )
+        self.rnn = nn.GRU(input_size=self.rnn_input_size,
+                          hidden_size=self.hidden_size,
+                          num_layers=self.nlayers,
+                          bidirectional=self.bidirectional,
+                          dropout=.1)
+
+        # define initial state
+        self.init_hidden = torch.nn.Parameter(torch.randn(self.nlayers * self.directions, 1, self.hidden_size))
+
+        # def key/value
+        hidden_size = self.hidden_size * 2 if self.bidirectional else self.hidden_size
+        self.scoring = nn.Sequential(
+            nn.BatchNorm1d(hidden_size),
+            nn.Sigmoid(),
+            nn.Linear(hidden_size, self.kv_size)
+        )
+
+        # initialize
+        for m in self.modules():
+            if isinstance(m, nn.Conv2d):
+                nn.init.kaiming_normal_(m.weight)
+            elif isinstance(m, nn.BatchNorm2d):
+                nn.init.constant_(m.weight, 1)
+                nn.init.constant_(m.bias, 0)
+
+    def forward(self, seq_list):
+        '''
+        :param seq_list: list of sequences ordered by size
+        :return: scores (sum(seq_lens), kv_size)
+        '''
+        batch_size = len(seq_list)
+        lens = [len(s) for s in seq_list]  # lens of all inputs (sorted by loader)
+        seq_list = rnn.pad_sequence(seq_list, batch_first=True)  # batch_size, max_len, features
+        seq_list = seq_list.cuda() if torch.cuda.is_available() else seq_list
+        seq_list = seq_list.unsqueeze(1)  # create a channel for CNN: batch_size, 1, max_len, features
+        embedding = self.cnn(seq_list)  # wasteful cnn computes over padded data: batch_size, 1, red_max_len, features
+        n, c, h, w = embedding.size()  # h is (CNN reduced) max_len; w * c are features
+        reduced_embeddings = []  # batch_size, reduced_len, features
+        mask = []
+        for i in range(batch_size):
+            l = -(-lens[i] // self.cnn_compression)  # ceiling
+            e = embedding[i, :, :l, :].permute(0, 2, 1).contiguous().view(c * w, l).permute(1,
+                                                                                            0)  # reduced_len, features
+            reduced_embeddings.append(e)
+            mask.append(l)
+        # reduced_embeddings = seq_list
+        # mask = lens
+        packed_input = rnn.pack_sequence(reduced_embeddings)  # packed uneven length sequences for fast RNN processing
+        packed_input = packed_input.cuda() if torch.cuda.is_available() else packed_input
+        # learn initial state
+        init_hidden = self.init_hidden.expand(-1, batch_size, -1)
+        packed_output, hidden = self.rnn(packed_input, init_hidden)  # reduced_len, batch_size, features
+        packed_output, _ = rnn.pad_packed_sequence(packed_output)  # unpacked output (padded)
+        output_flatten = torch.cat(
+            [packed_output[:mask[i], i] for i in
+             range(batch_size)])  # concatenated output (batch_size, sum(reduced_len), hidden)
+        scores_flatten = self.scoring(output_flatten)  # concatenated scores (batch_size, sum(reduced_len), kv_size)
+        cum_lens = np.cumsum([0] + mask)
+        scores_unflatten = [scores_flatten[cum_lens[i]:cum_lens[i + 1]] for i in range(batch_size)]
+        scores_unflatten = rnn.pad_sequence(scores_unflatten)  # max_len, batch, kv_size
+        key = scores_unflatten[:, :, :self.key_size]  # max_len, batch, key_size
+        value = scores_unflatten[:, :, self.key_size:]  # max_len, batch, value_size
+        return key, value, hidden, mask  # return concatenated key, value, hidden state, mask
+
+
+# Model that takes packed sequences in training
+class DecoderRNN(nn.Module):
+    def __init__(self, num_chars, key_size=128, value_size=256, nlayers=4,
+                 hidden_size=512, input_size=31, teacher=0.4, bidirectional=False):
+        super(DecoderRNN, self).__init__()
+        self.teacher = teacher
+        self.key_size = key_size
+        self.value_size = value_size
+        self.num_chars = num_chars
+        self.bidirectional = bidirectional
+        self.bidirectional = False
+        self.directions = 2 if bidirectional else 1
+        self.hidden_size = hidden_size
+        self.nlayers = nlayers
+
+        # create RNN
+        self.init_hidden = []
+        self.cells = []
+        for i in range(self.nlayers):
+            if i == 0:
+                input_size = self.num_chars
+                hidden_size = self.hidden_size + self.value_size
+            elif i == 1:
+                input_size = self.hidden_size + self.value_size
+                hidden_size = self.hidden_size
+            else:
+                input_size = hidden_size = self.hidden_size
+            self.init_hidden.append(torch.nn.Parameter(torch.randn((1, hidden_size))))
+            self.cells.append(torch.nn.GRUCell(input_size, hidden_size, bias=True))
+
+        # create attention
+        self.query = nn.Linear(self.hidden_size, self.key_size)
+
+        # create scoring
+        hidden_size = self.hidden_size * 2 if self.bidirectional else self.hidden_size
+        self.scoring = nn.Sequential(
+            nn.BatchNorm1d(hidden_size),
+            nn.Sigmoid(),
+            nn.Linear(hidden_size, self.hidden_size),
+            nn.BatchNorm1d(self.hidden_size),
+            nn.Sigmoid(),
+            nn.Linear(self.hidden_size, self.num_chars)
+        )
+
+        # initialize
+        for m in self.modules():
+            if isinstance(m, nn.Conv2d):
+                nn.init.kaiming_normal_(m.weight)
+            elif isinstance(m, nn.BatchNorm2d):
+                nn.init.constant_(m.weight, 1)
+                nn.init.constant_(m.bias, 0)
+
+    def forward(self, seq_list, keys, values, masks):
+
+        batch_size = len(seq_list)
+        lens = [len(s) for s in seq_list]  # lens of all inputs (sorted by loader)
+        seq_list = rnn.pad_sequence(seq_list, batch_first=True)  # batch_size, max_len, features
+        seq_list = seq_list.cuda() if torch.cuda.is_available() else seq_list
+
+        hiddens = []
+        for hidden in self.init_hidden:
+            hiddens.append(hidden.expand(batch_size, -1))
+
+        matrix_mask = torch.zeros(keys.shape[1], 1, keys.shape[0])
+        for i, mask in enumerate(masks):
+            matrix_mask[i, 0, :mask] = 1
+
+        rnn_pred = []
+        for t in range(lens[0]):
+            # teacher forcing
+            if not (self.teacher > np.random.random() and t != 0):
+                x = seq_list[:, t]
+
+            query = self.query(hiddens[-1]).unsqueeze(0)  # 1, batch_size, hidden_size
+
+            # attention calculation
+            # Your key and query needs to have the same dim as you multiply (1,128) Q with (128,T)
+            # key for getting the energies for all timesteps. Once you have that (1,T), you simply
+            # multiply them with your values which are (T,V) to get a context of (1,V). As mentioned
+            # before "V" has no hard constraints.
+
+            # query: 1, batch_size, hidden_size || keys: max_len, batch_size, key_size
+            energy = torch.bmm(query.permute(1, 0, 2), keys.permute(1, 2, 0))  # batch_size, 1, max_len
+            attention = F.softmax(energy, dim=2)  # along seq_len: batch_size, 1, max_len
+            attention = attention * matrix_mask  # mask attention
+            attention = F.normalize(attention, p=1, dim=2)
+            # context: values: max_len, batch_size, value_size
+            context = torch.bmm(attention, values.permute(1, 0, 2))  # batch_size, 1, value_size
+            context = context.permute(1, 0, 2)  # 1, batch_size, value_size
+
+            hiddens[0][:, :context.shape[2]] = context.squeeze(0)
+            for i, cell in enumerate(self.cells):
+                x = hiddens[i - 1] if i > 0 else x
+                hiddens[i] = cell(x, hiddens[i])
+                out = hiddens[i]
+                # teacher forcing
+                x = F.gumbel_softmax(self.scoring(out), hard=True) if i == len(self.cells) - 1 else x
+            rnn_pred.append(out)
+
+        rnn_pred = torch.stack(rnn_pred)
+        output_flatten = torch.cat(
+            [rnn_pred[:lens[i], i] for i in
+             range(batch_size)])  # concatenated output (sum(lens), hidden)
+        scores_flatten = self.scoring(output_flatten)  # concatenated scores (sum(lens), num_chars)
+        cum_lens = np.cumsum([0] + lens)
+        scores_unflatten = [scores_flatten[cum_lens[i]:cum_lens[i + 1]] for i in range(batch_size)]
+        scores_unflatten = rnn.pad_sequence(scores_unflatten, padding_value=-99)  # max_len, batch, num_chars
+        return scores_unflatten
+
+
+class LAS(nn.Module):
+    def __init__(self, num_chars=32, key_size=128, value_size=256, encoder_depth=3, decoder_depth=4, encoder_hidden=512,
+                 decoder_hidden=512, cnn_compression=2, enc_bidirectional=True, teacher=0.4):
+        super(LAS, self).__init__()
+        self.encoder = EncoderCNN(key_size=key_size, value_size=value_size, nlayers=encoder_depth,
+                                  hidden_size=encoder_hidden, cnn_compression=cnn_compression,
+                                  bidirectional=enc_bidirectional)
+        self.decoder = DecoderRNN(num_chars=num_chars, key_size=key_size, value_size=value_size, nlayers=decoder_depth,
+                                  hidden_size=decoder_hidden, teacher=teacher, bidirectional=False)
+
+    def forward(self, input, target):
+        enc_out = self.encoder(input)
+        out = self.decoder(target, enc_out[0], enc_out[1], enc_out[3])
+        return out
+
+
+if __name__ == '__main__':
+    import character_list
+
+    enc = EncoderCNN(cnn_compression=2)
+    dec = DecoderRNN(len(character_list.LETTERS) + 1)
+    las = LAS()
+
+    with torch.no_grad():
+        enc_out = enc([torch.ones((120, 40)), torch.ones((90, 40))])
+        print(enc_out[0].shape)
+        print(dec([torch.ones((4, 32)), torch.ones((2, 32))], enc_out[0], enc_out[1], enc_out[3]).shape)
+        print(las([torch.ones((120, 40)), torch.ones((90, 40))],
+                  [torch.ones((4, 32)), torch.ones((2, 32))]).shape)
---
+++ train.py
@@ -0,0 +1,340 @@
+import torch
+import numpy as np
+from torch.utils.data import DataLoader, Dataset
+import torch.nn.functional as F
+import character_list
+# import Levenshtein as L
+import logging
+from model import LAS
+import pdb
+
+torch.multiprocessing.set_sharing_strategy('file_system')
+logging.basicConfig(filename='train.log', level=logging.DEBUG)
+
+
+# data loader
+class UtteranceDataset(Dataset):
+    def __init__(self, data_path='./data/dev.npy', label_path='./data/dev_transcripts.npy', test=False):
+        self.letter_dict = {j: i + 1 for i, j in enumerate(character_list.LETTERS)}
+        self.letter_dict['<>'] = 0
+        self.test = test
+        self.data = np.load(data_path, encoding='latin1')
+        labels = np.load(label_path) if not test else None  # index labels from 1 to n_labels
+        self.labels = []
+        for words in labels:
+            words = np.array([0]
+                             + [self.letter_dict[letter] for letter in ' '.join(words.astype(str)) if letter != '_']
+                             + [0])
+            self.labels.append(words)
+        self.labels = np.array(self.labels)
+        self.num_entries = len(self.data)
+        self.num_entries = int(len(self.data)*.001) if not 'test' in data_path else int(len(self.data)*.1)
+
+    def __getitem__(self, i):
+        data = self.data[i]
+        data = torch.from_numpy(data)
+        if self.test:
+            return data
+        else:
+            labels = self.labels[i]
+            labels = torch.from_numpy(labels)
+            return data, labels
+
+    def __len__(self):
+        return self.num_entries
+
+
+def collate(batch):
+    '''
+    Collate function. Transform a list of different length sequences into a batch. Passed as an argument to the DataLoader.
+    seq_list: list with size batch_size. Each element is a tuple where the first element is the predictor
+    data and the second element is the label.
+    output: data on format (batch_size, var_len_sequence)
+    '''
+    if len(batch[0]) == 2:
+        utts, labels = zip(*batch)
+        lens = [seq.size(0) for seq in utts]
+        seq_order = sorted(range(len(lens)), key=lens.__getitem__, reverse=True)
+        utts = [utts[i] for i in seq_order]
+        labels = [labels[i] for i in seq_order]
+        return utts, labels
+    else:
+        utts = batch
+        lens = [seq.size(0) for seq in utts]
+        seq_order = sorted(range(len(lens)), key=lens.__getitem__, reverse=True)
+        utts = [utts[i] for i in seq_order]
+        return utts
+
+
+class Levenshtein:
+    def __init__(self, charmap):
+        self.label_map = ['<>'] + charmap  # add special char to first entry
+
+    def __call__(self, prediction, target):
+        return self.forward(prediction, target)
+
+    def forward(self, prediction, target):
+        ls = 0.
+        for i in range(len(target)):
+            pred = "".join(self.label_map[j-1] for j in prediction[i].numpy())
+            true = "".join(self.label_map[j-1] for j in target[i].numpy())
+            ls += L.distance(pred, true)
+        return ls
+
+
+# model trainer
+class LanguageModelTrainer:
+    def __init__(self, model, loader, val_loader, test_loader, max_epochs=1, chars=character_list.LETTERS):
+        self.model = model.cuda() if torch.cuda.is_available() else model
+        self.chars = chars
+        self.loader = loader
+        self.val_loader = val_loader
+        self.test_loader = test_loader
+        self.train_losses = []
+        self.val_losses = []
+        self.predictions = []
+        self.predictions_test = []
+        self.generated_logits = []
+        self.generated = []
+        self.generated_logits_test = []
+        self.generated_test = []
+        self.epochs = 0
+        self.max_epochs = max_epochs
+
+        self.optimizer = torch.optim.Adam(model.parameters(), lr=1e-3, weight_decay=1e-6)
+        # self.optimizer = torch.optim.SGD(model.parameters(), lr=0.0001, weight_decay=1e-6, momentum=0.9)
+        self.criterion = torch.nn.CrossEntropyLoss(reduction='elementwise_mean', ignore_index=-99)
+        self.criterion = self.criterion.cuda() if torch.cuda.is_available() else self.criterion
+        self.scheduler = torch.optim.lr_scheduler.ReduceLROnPlateau(self.optimizer, factor=0.1, patience=2)
+        self.LD = Levenshtein(self.chars)
+        self.best_rate = 1e10
+
+    def train(self):
+        self.model.train()  # set to training mode
+        for epoch in range(self.max_epochs):
+            epoch_loss = 0
+            training_epoch_loss = 0
+            for batch_num, (inputs, targets) in enumerate(self.loader):
+                # # debug
+                # # Save init values
+                # old_state_dict = {}
+                # for key in model.state_dict():
+                #     old_state_dict[key] = model.state_dict()[key].clone()
+                #
+                # # Your training procedure
+                # loss = self.train_batch(inputs, targets)
+                #
+                # # Save new params
+                # new_state_dict = {}
+                # for key in model.state_dict():
+                #     new_state_dict[key] = model.state_dict()[key].clone()
+                #
+                # # Compare params
+                # for key in old_state_dict:
+                #     if (old_state_dict[key] == new_state_dict[key]).all():
+                #         print('No diff in {}'.format(key))
+                # print('Batch loss is ', float(loss))
+
+                targets = torch.nn.utils.rnn.pad_sequence(targets, padding_value=-99)
+                targets = targets.cuda() if torch.cuda.is_available() else targets
+                inputs = inputs.cuda() if torch.cuda.is_available() else inputs
+
+                loss = self.train_batch(inputs, targets)
+                epoch_loss += loss
+                training_epoch_loss += loss
+                # training print
+                batch_print = 40
+                if batch_num % batch_print == 0 and batch_num != 0:
+                    self.print_training(batch_num, self.loader.batch_size, training_epoch_loss, batch_print)
+                    training_epoch_loss = 0
+
+            epoch_loss = epoch_loss / (batch_num + 1)
+            self.epochs += 1
+            self.scheduler.step(epoch_loss)
+            print('[TRAIN]  Epoch [%d/%d]   Loss: %.4f'
+                  % (self.epochs, self.max_epochs, epoch_loss))
+            self.train_losses.append(epoch_loss)
+            # log loss
+            tLog.log_scalar('training_loss', epoch_loss, self.epochs)
+            # log values and gradients of parameters (histogram summary)
+            for tag, value in self.model.named_parameters():
+                tag = tag.replace('.', '/')
+                tLog.log_histogram(tag, value.data.cpu().numpy(), self.epochs)
+                tLog.log_histogram(tag + '/grad', value.grad.data.cpu().numpy(), self.epochs)
+            # save
+            torch.save(self.model.state_dict(), "models/{}.pt".format(epoch))
+
+            # every 1 epochs, print validation statistics
+            epochs_print = 1
+            if self.epochs % epochs_print == 0 and not self.epochs == 0:
+                with torch.no_grad():
+                    t = "#########  Epoch {} #########".format(self.epochs)
+                    print(t)
+                    logging.info(t)
+                    ls = 0
+                    lens = 0
+                    for j, (val_inputs, val_labels) in (enumerate(self.val_loader)):
+                        idx = np.random.randint(0, len(val_inputs))
+                        print('Ground', ''.join([self.chars[j-1] for j in val_labels[idx]]))
+                        val_output, feature_lengths = self.gen_greedy_search(val_inputs, 190)
+                        print('Pred', ''.join([self.chars[j-1] for j in val_output[idx:idx + 1]]))
+                        ls += self.LD.forward(val_output, val_labels)
+                        lens += len(val_inputs)
+                    ls /= lens
+                    t = "Validation LD {}:".format(ls)
+                    print(t)
+                    logging.info(t)
+                    t = '--------------------------------------------'
+                    print(t)
+                    logging.info(t)
+                    # log loss
+                    vLog.log_scalar('LD', ls, self.epochs)
+                    if self.best_rate > ls:
+                        torch.save(self.model.state_dict(), "models/best.pt")
+                        self.best_rate = ls
+
+    def print_training(self, batch_num, batch_size, loss, batch_print):
+        t = 'At {:.0f}% of epoch {}'.format(
+            batch_num * batch_size / self.loader.dataset.num_entries * 100, self.epochs)
+        print(t)
+        logging.info(t)
+        t = "Training perplexity: {}".format(np.exp(loss / batch_print))
+        print(t)
+        logging.info(t)
+        t = '--------------------------------------------'
+        print(t)
+        logging.info(t)
+
+    def train_batch(self, inputs, targets):
+        scores = self.model(inputs, targets)
+        loss = self.criterion(scores, targets[:, 1:])
+        loss.backward()
+        self.optimizer.step()
+        self.optimizer.zero_grad()
+        # print([i for i in model.cnn.modules()][1].__dict__['_parameters']['weight'][0])
+        return float(loss)  # avoid autograd retention
+
+    def test(self):
+        preds = []
+        for i, inputs in enumerate(self.test_loader):
+            pred = self.gen_random_search(inputs, 100, 190)
+            pred = [self.chars[j - 1] for j in pred]
+            preds.append(pred)
+        return preds
+
+    def gen_greedy_search(self, data_batch, max_len):
+        prediction = []  # store predictions
+        enc_out = self.model.decoder(data_batch)
+        starts = torch.zeros(1, 31)
+        prediction.append(starts)
+        scores = self.model.decoder(starts, enc_out[0], enc_out[1], enc_out[3])
+        for i in range(max_len-1):
+            words = torch.argmax(scores, dim=1)
+            prediction.append(words)
+            scores = self.model.decoder(words, enc_out[0], enc_out[1], enc_out[3])
+        prediction = torch.stack(prediction, dim=1)
+
+        # remove excess words
+        lens = torch.argmin(prediction, dim=1).tolist()  # finds the 0s in the prediction
+        assert len(lens) == len(prediction), 'lens and prediction dont match'
+        prediction = [prediction[i, :lens[i]] for i in range(len(prediction))]
+        seq_order = sorted(range(len(lens)), key=lens.__getitem__, reverse=True)
+        prediction = [prediction[i] for i in seq_order]
+        return prediction, lens
+
+    def gen_random_search(self, data_batch, random_paths, max_len):
+        loss = torch.nn.CrossEntropyLoss(reduction='sum')
+
+        enc_out = self.model.decoder(data_batch)
+        starts = torch.zeros(1, len(self.chars)+1)
+        prediction = []  # store predictions
+        losses = []
+        for iter in range(random_paths):
+            rand_pred = list()  # store batch_preds
+            rand_pred.append(starts)
+            scores = self.model.decoder(starts, enc_out[0], enc_out[1], enc_out[3])
+            for i in range(max_len-1):
+                scores = F.softmax(scores, dim=1)
+                words = torch.multinomial(scores, 1, replacement=False)
+                rand_pred.append(words)
+                scores = self.model.decoder(words, enc_out[0], enc_out[1], enc_out[3])
+            rand_pred = torch.stack(rand_pred, dim=1)
+            lens = torch.argmin(rand_pred, dim=1).tolist()  # finds the 0s in the prediction
+            assert len(lens) == len(rand_pred), 'lens and prediction dont match'
+            rand_pred = [rand_pred[i, :lens[i]] for i in range(len(rand_pred))]
+            seq_order = sorted(range(len(lens)), key=lens.__getitem__, reverse=True)
+            rand_pred = [rand_pred[i] for i in seq_order]
+            scores = self.model.decoder(rand_pred, enc_out[0], enc_out[1], enc_out[3])
+            prediction.append(rand_pred)
+            rand_pred = torch.nn.utils.rnn.pad_sequence(rand_pred, batch_first=True, padding_value=-99)
+            loss = float(loss(scores, rand_pred[:, 1:], ignore_index=-99))
+            losses.append(loss)
+
+        losses = torch.stack(losses, dim=1)
+        m, argminloss = torch.min(losses, dim=1)
+        prediction = [prediction[idx_best][i] for i, idx_best in enumerate(argminloss)]
+        return prediction
+
+
+    # def old_batch(self, data_batch):
+    #     scores, _, out_lengths = model(data_batch)
+    #     out_lengths = torch.Tensor(out_lengths)
+    #     scores = torch.transpose(scores, 0, 1)
+    #     probs = F.softmax(scores, dim=2).data.cpu()
+    #     output, scores, timesteps, out_seq_len = self.decoder.decode(probs=probs, seq_lens=out_lengths)
+    #     out_seq = []
+    #     for i in range(output.size(0)):
+    #         chrs = [character_list.LETTERS[o.item() - 1] for o in output[i, 0, :out_seq_len[i, 0]]]
+    #         out_seq.append("".join(chrs))
+    #     return out_seq
+
+
+def write_results(results):
+    with open('predictions.csv', 'w') as f:
+        f.write('Id,Predicted\n')
+        for i, r in enumerate(results):
+            f.write(','.join([str(i), r]))
+            f.write('\n')
+
+if __name__ == '__main__':
+    import os.path
+    import logger
+
+    tLog, vLog = logger.Logger("./logs/train_pytorch"), logger.Logger("./logs/val_pytorch")
+
+    NUM_EPOCHS = 1
+    BATCH_SIZE = 64
+
+    model = model.LAS(num_chars=32, key_size=128, value_size=256, encoder_depth=3, decoder_depth=4, encoder_hidden=512,
+                 decoder_hidden=512, cnn_compression=2, enc_bidirectional=True, teacher=0.0)
+
+    def load_my_state_dict(net, state_dict):
+        own_state = net.state_dict()
+        for name, param in state_dict.items():
+            if name not in own_state:
+                continue
+            if isinstance(param, torch.nn.Parameter):
+                # backwards compatibility for serialized parameters
+                param = param.data
+            own_state[name].copy_(param)
+        return net
+
+
+    ckpt_path = 'models/best.pt'
+    if os.path.isfile(ckpt_path):
+        pretrained_dict = torch.load(ckpt_path, map_location=lambda storage, loc: storage)
+        model = load_my_state_dict(model, pretrained_dict)
+        print('Checkpoint weights loaded.')
+
+    utdst = UtteranceDataset(data_path=r'.\data\train.npy', label_path=r'.\data\train_transcripts.npy')
+    val_utdst = UtteranceDataset(data_path=r'.\data\dev.npy', label_path=r'.\data\dev_transcripts.npy')
+    test_utdst = UtteranceDataset(r'.\data\test.npy', test=True)
+    loader = DataLoader(dataset=utdst, batch_size=BATCH_SIZE, shuffle=True, collate_fn=collate, num_workers=6)
+    val_loader = DataLoader(dataset=val_utdst, batch_size=BATCH_SIZE, shuffle=False, collate_fn=collate, num_workers=6)
+    test_loader = DataLoader(dataset=test_utdst, batch_size=1, shuffle=False, collate_fn=collate, num_workers=1)
+
+    trainer = LanguageModelTrainer(model=model, loader=loader, val_loader=val_loader,
+                                   test_loader=test_loader, max_epochs=NUM_EPOCHS)
+
+    trainer.train()
+    write_results(trainer.test())
