4b62735445d0565b16e22a57245ab032b5b5bc94
Aldo Marini <34013890+catapulta@users.noreply.github.com>
2018-12-08 14:18:27 -0800

    update readme and clean-up

--- data_analyzer.py
+++ data_analyzer.py
@@ -4,23 +4,11 @@ from collections import Counter
 x = np.load(r'.\data\dev_transcripts.npy', encoding='latin1')
 x = np.concatenate([x, np.load(r'.\data\train_transcripts.npy', encoding='latin1')])
 
-# concat = ''
-# word_count = Counter()
 letter_count = Counter()
 rows = []
 for words in x:
-#     word_count.update(words)
     rows.append(len(' '.join(words.astype(str))))
     letter_count.update(' '.join(words.astype(str)))
 rows = np.array(rows)
 print(np.percentile(rows, 95))
 print(rows.max())
-#
-# print(word_count)
-# print(letter_count)
-# print(len(letter_count))
-#
-# print(list(letter_count.keys()))
-
-# x = np.load(r'.\data\dev.npy', encoding='latin1')
-# print(x[0].shape)
